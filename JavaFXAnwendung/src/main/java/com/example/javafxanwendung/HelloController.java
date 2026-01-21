6ff927cc5fb2485fd2e8a05f0ef89e1112fc255f
AchleitnerAlex <aachleit@htl-steyr.ac.at>
2026-01-21 12:12:05 +0100

    Database added

--- JavaFXAnwendung/src/main/java/com/example/javafxanwendung/HelloController.java
+++ JavaFXAnwendung/src/main/java/com/example/javafxanwendung/HelloController.java
@@ -2,10 +2,23 @@ package com.example.javafxanwendung;
 
 import javafx.fxml.FXML;
 import javafx.scene.control.Label;
+import java.sql.*;
+import java.util.Properties;
 
 public class HelloController {
     @FXML
     private Label welcomeText;
+    public HelloController() {
+        String url = "jdbc:postgresql://xserv:5432/world2";
+        Properties props = new Properties();
+        props.setProperty("user", "reader");
+        props.setProperty("password", "reader");
+        try {
+            Connection conn = DriverManager.getConnection(url, props);
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+    }
 
     @FXML
     protected void onHelloButtonClick() {
--- JavaFXAnwendung/src/main/java/module-info.java
+++ JavaFXAnwendung/src/main/java/module-info.java
@@ -1,6 +1,7 @@
 module com.example.javafxanwendung {
     requires javafx.controls;
     requires javafx.fxml;
+    requires java.sql;
 
 
     opens com.example.javafxanwendung to javafx.fxml;
