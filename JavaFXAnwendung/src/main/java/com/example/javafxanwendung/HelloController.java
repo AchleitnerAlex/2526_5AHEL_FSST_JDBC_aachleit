89cca5533b4fff3d733a5197b5b5308026814c98
AchleitnerAlex <aachleit@htl-steyr.ac.at>
2026-01-22 10:06:24 +0100

    JavaFX Update

--- JavaFXAnwendung/src/main/java/com/example/javafxanwendung/HelloController.java
+++ JavaFXAnwendung/src/main/java/com/example/javafxanwendung/HelloController.java
@@ -1,27 +1,46 @@
 package com.example.javafxanwendung;
 
 import javafx.fxml.FXML;
-import javafx.scene.control.Label;
+import javafx.scene.chart.BarChart;
+import javafx.scene.control.Slider;
+import javafx.scene.input.MouseEvent;
+
 import java.sql.*;
 import java.util.Properties;
 
 public class HelloController {
-    @FXML
-    private Label welcomeText;
+
+    public Slider sl_amountOfCountry;
+    protected Connection conn;
+
+    public BarChart bc_amountcity;
+
     public HelloController() {
         String url = "jdbc:postgresql://xserv:5432/world2";
         Properties props = new Properties();
         props.setProperty("user", "reader");
         props.setProperty("password", "reader");
         try {
-            Connection conn = DriverManager.getConnection(url, props);
+            conn = DriverManager.getConnection(url, props);
         } catch (SQLException e) {
             throw new RuntimeException(e);
         }
     }
 
-    @FXML
-    protected void onHelloButtonClick() {
-        welcomeText.setText("Welcome to JavaFX Application!");
+    public void Initialize() throws SQLException {
+        sl_amountOfCountry.adjustValue(1);
+
+
+    }
+
+    public void changedAmouCountry(MouseEvent mouseEvent) {
+
+    }
+
+    protected void chartupdate(int amoOfCountry) throws SQLException {
+        Statement st = conn.createStatement();
+
+
     }
 }
+
